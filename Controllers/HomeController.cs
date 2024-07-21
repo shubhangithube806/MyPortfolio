d1ade4af4d0dab261bd8be8d9592b3d9793e3b2c
Shubhangi <shubhangithube806@gmail.com>
2024-07-21 14:26:22 +0530

    Remove default about and home pages.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,18 +13,8 @@ namespace MyPortfolio.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+      
 
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+       
     }
 }
