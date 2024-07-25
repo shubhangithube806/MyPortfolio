6d1b938c3639cc6b0033d7d839b95fc8f216dc61
Shubhangi <shubhangithube806@gmail.com>
2024-07-25 17:55:13 +0530

    Create Helpers class in CommonFile folder and add GetPortfolioUserId method in it.

---
+++ CommonFiles/Helpers.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Identity;
+using MyPortfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+
+namespace MyPortfolio.CommonFiles
+{
+    public class Helpers
+    {
+        public static Guid GetPortfolioUserId(IPrincipal user)
+        {
+            using(ApplicationDbContext db = new ApplicationDbContext())
+            {
+                Guid userId = new Guid(user.Identity.GetUserId());
+
+                PortfolioUser portfolioUser = db.PortfolioUser.Where(m => m.UserId == userId).FirstOrDefault();
+
+                return portfolioUser.PortfolioUserId;
+            }
+
+        }
+    }
+}
