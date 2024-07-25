5cec05e63a657f531878afebaebad27864db9dcd
Shubhangi <shubhangithube806@gmail.com>
2024-07-25 18:00:19 +0530

    Add CreatePortfolioViewModel class in ViewModel

--- Controllers/CreatePortfolioController.cs
+++ Controllers/CreatePortfolioController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MyPortfolio.CommonFiles;
+using MyPortfolio.Models;
+using MyPortfolio.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +9,18 @@ using System.Web.Mvc;
 
 namespace MyPortfolio.Controllers
 {
+    [Authorize]
     public class CreatePortfolioController : Controller
     {
+        ApplicationDbContext db = new ApplicationDbContext();
         // GET: CreatePortfolio
         public ActionResult Index()
         {
-            return View();
+            CreatePortfolioViewModel model = new CreatePortfolioViewModel();
+
+            model.Init(db, Helpers.GetPortfolioUserId(User));
+          
+            return View(model);
         }
     }
 }
---
+++ ViewModels/CreatePortfolioViewModel.cs
@@ -0,0 +1,20 @@
+﻿using MyPortfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyPortfolio.ViewModels
+{
+    public class CreatePortfolioViewModel
+    {
+        public Guid BasicInfoId { get; set; }
+
+        public void Init(ApplicationDbContext db, Guid portfolioUserId)
+        {
+
+            this.BasicInfoId = db.BasicInfo.Where(m => m.PortfolioUserId == portfolioUserId).Select(m => m.BasicInfoId).FirstOrDefault();
+
+        }
+    }
+}
