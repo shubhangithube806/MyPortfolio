d5b5026ca06869eb45c6775341693cc6cf0e328b
Shubhangi <shubhangithube806@gmail.com>
2024-07-27 16:39:17 +0530

    Add validations to BasicInfo form.

---
+++ Migrations/202407271101526_AddDataAnnotationsToBasicInfo..cs
@@ -0,0 +1,30 @@
+﻿namespace MyPortfolio.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddDataAnnotationsToBasicInfo : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.BasicInfo", "Name", c => c.String(nullable: false, maxLength: 50));
+            AlterColumn("dbo.BasicInfo", "Address", c => c.String(nullable: false, maxLength: 50));
+            AlterColumn("dbo.BasicInfo", "PhoneNumber", c => c.String(nullable: false, maxLength: 15));
+            AlterColumn("dbo.BasicInfo", "BirthDate", c => c.DateTime(nullable: false));
+            AlterColumn("dbo.BasicInfo", "Designation", c => c.String(nullable: false, maxLength: 50));
+            AlterColumn("dbo.BasicInfo", "Email", c => c.String(nullable: false, maxLength: 50));
+            AlterColumn("dbo.BasicInfo", "Profile", c => c.String(nullable: false, maxLength: 500));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.BasicInfo", "Profile", c => c.String());
+            AlterColumn("dbo.BasicInfo", "Email", c => c.String());
+            AlterColumn("dbo.BasicInfo", "Designation", c => c.String());
+            AlterColumn("dbo.BasicInfo", "BirthDate", c => c.DateTime());
+            AlterColumn("dbo.BasicInfo", "PhoneNumber", c => c.String());
+            AlterColumn("dbo.BasicInfo", "Address", c => c.String());
+            AlterColumn("dbo.BasicInfo", "Name", c => c.String());
+        }
+    }
+}
--- Models/BasicInfo.cs
+++ Models/BasicInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -11,18 +12,34 @@ namespace MyPortfolio.Models
 
         public Guid PortfolioUserId { get; set; }
 
+        [Required]
+        [StringLength(50)]
         public string Name { get; set; }
 
+        [Required]
+        [StringLength(50)]
         public string Address { get; set; }
 
+        [Required]
+        [StringLength(15, ErrorMessage = "Phone number must be no more than {1} digits.")]
+        [Phone]
+        [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
 
+        [Required]
         public DateTime? BirthDate { get; set; }
 
+        [Required]
+        [StringLength(50)]
         public string Designation { get; set; }
 
+        [Required]
+        [StringLength(50)]
+        [EmailAddress]
         public string Email { get; set; }
 
+        [Required]
+        [StringLength(500)]
         public string Profile { get; set; }
 
     }
