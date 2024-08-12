0f2b35e0a4eb6c40c0ed46513366345284a28014
Shubhangi <shubhangithube806@gmail.com>
2024-08-12 15:07:22 +0530

    Add Education table.

---
+++ Controllers/MyEducationController.cs
@@ -0,0 +1,66 @@
+﻿using MyPortfolio.CommonFiles;
+using MyPortfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPortfolio.Controllers
+{
+    [Authorize]
+    public class MyEducationController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        // GET: MyEducation
+        [HttpGet]
+        public ActionResult SaveEducation(Guid? educationId)
+        {
+            Education education = new Education();
+
+            if (educationId != null)
+            {
+                education = db.Education.Where(m => m.EducationId == educationId).FirstOrDefault();
+            }
+            return View(education);
+        }
+
+        [HttpPost]
+        public ActionResult SaveEducation(Education education)
+        {
+            if (ModelState.IsValid)
+            {
+                if (education.EducationId == Guid.Empty)
+                {
+                    //Add Education
+                    education.EducationId = Guid.NewGuid();
+                    education.PortfolioUserId = Helpers.GetPortfolioUserId(User);
+
+                    db.Education.Add(education);
+                    db.SaveChanges();
+                }
+                else
+                {
+                    //Edit Education
+                    db.Entry(education).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+                }
+
+                return RedirectToAction("Index", "CreatePortfolio");
+            }
+
+            return View(education);
+
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- ViewModels/CreatePortfolioViewModel.cs
+++ ViewModels/CreatePortfolioViewModel.cs
@@ -10,10 +10,14 @@ namespace MyPortfolio.ViewModels
     {
         public Guid BasicInfoId { get; set; }
 
+        public Guid EducationId { get; set; }
+
         public void Init(ApplicationDbContext db, Guid portfolioUserId)
         {
 
             this.BasicInfoId = db.BasicInfo.Where(m => m.PortfolioUserId == portfolioUserId).Select(m => m.BasicInfoId).FirstOrDefault();
+
+            this.EducationId = db.Education.Where(m => m.PortfolioUserId == portfolioUserId).Select(m => m.EducationId).FirstOrDefault();
 
         }
     }
