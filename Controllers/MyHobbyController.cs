c8e15ce46d2d69b4b2e44a0bb769f8f2ada96256
Shubhangi <shubhangithube806@gmail.com>
2024-09-02 15:55:00 +0530

    Functionality added for Hobby.

---
+++ Controllers/MyHobbyController.cs
@@ -0,0 +1,89 @@
+﻿using MyPortfolio.CommonFiles;
+using MyPortfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPortfolio.Controllers
+{
+    [Authorize]
+    public class MyHobbyController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+
+        public ActionResult Index()
+        {
+            Guid portfolioUserId = Helpers.GetPortfolioUserId(User);
+
+            List<Hobby> hobbyList = db.Hobby.Where(m => m.PortfolioUserId == portfolioUserId).ToList();
+
+            return View(hobbyList);
+        }
+
+        // GET: MyHobby
+        [HttpGet]
+        public ActionResult SaveHobby(Guid? hobbyId)
+        {
+            Hobby hobby = new Hobby();
+
+            if (hobbyId != null)
+            {
+                hobby = db.Hobby.Where(m => m.HobbyId == hobbyId).FirstOrDefault();
+            }
+            return View(hobby);
+        }
+
+
+        [HttpPost]
+        public ActionResult SaveHobby(Hobby hobby)
+        {
+            if (ModelState.IsValid)
+            {
+                if (hobby.HobbyId == Guid.Empty)
+                {
+                    //Add Hobby
+                    hobby.HobbyId = Guid.NewGuid();
+                    hobby.PortfolioUserId = Helpers.GetPortfolioUserId(User);
+
+                    db.Hobby.Add(hobby);
+                    db.SaveChanges();
+                }
+                else
+                {
+                    //Edit Hobby
+                    db.Entry(hobby).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+                }
+
+                return RedirectToAction("Index", "MyHobby");
+            }
+
+            return View(hobby);
+        }
+
+        public ActionResult DeleteHobby(Guid hobbyId)
+        {
+            Hobby hobby = db.Hobby.Where(e => e.HobbyId == hobbyId).FirstOrDefault();
+
+            if (hobby != null)
+            {
+                db.Hobby.Remove(hobby);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index", "MyHobby");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+    }
+}
--- ViewModels/CreatePortfolioViewModel.cs
+++ ViewModels/CreatePortfolioViewModel.cs
@@ -9,14 +9,13 @@ namespace MyPortfolio.ViewModels
     public class CreatePortfolioViewModel
     {
         public Guid BasicInfoId { get; set; }
-
         public int EducationCount { get; set; }
         public int ExperienceCount { get; set; }
         public int CourseCount { get; set; }
         public int SkillCount { get; set; }
         public int LanguageCount { get; set; }
-
         public int StrengthCount { get; set; }
+        public int HobbyCount { get; set; }
 
 
         public void Init(ApplicationDbContext db, Guid portfolioUserId)
@@ -35,6 +34,8 @@ namespace MyPortfolio.ViewModels
             this.LanguageCount = db.Language.Where(m => m.PortfolioUserId == portfolioUserId).Count();
 
             this.StrengthCount = db.Strength.Where(m => m.PortfolioUserId == portfolioUserId).Count();
+
+            this.HobbyCount = db.Hobby.Where(m => m.PortfolioUserId == portfolioUserId).Count();
 
         }
     }
